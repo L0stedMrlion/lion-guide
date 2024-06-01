4f4283da0b1f252a033fc2ef7fbfe8bc262a380e
Mrlion <mrlion4202@gmail.com>
2024-06-01 07:35:42 +0000

    added some things

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,71 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
-const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+function useNextSeoProps() {
+  const { asPath } = useRouter();
+  const arr = asPath.replace(/[-_]/g, " ").split("/");
+  const category = (arr[1][0] !== "#" && arr[1]) || "Lion Guide";
+  const rawTitle = arr[arr.length - 1];
+  const title =
+    /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : "%s";
+
+  return {
+    titleTemplate: `${title} - ${
+      rawTitle === category
+        ? "Documentation"
+        : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+    }`,
+  };
 }
 
-export default config
+function useHead() {
+  const { asPath } = useRouter();
+  const { frontMatter, title } = useConfig();
+  const url = `https://overextended.dev${asPath}`;
+  const description = frontMatter.description || "Guide for Mrlions Scripts";
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="lionsproject_logo.ico" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+    </>
+  );
+}
+
+const config: DocsThemeConfig = {
+  logo: (
+    <div
+      style={{
+        paddingLeft: "50px",
+        lineHeight: "38px",
+        background:
+          "url('https://cdn.discordapp.com/attachments/1178330264263462943/1229136630045081691/lionsproject_logo.png?ex=665c1170&is=665abff0&hm=83c3a6172f3893a7b93b6a372fa6fd8ef6f334758bbba487dd2872e58d0950dc&') no-repeat left",
+        backgroundSize: "38px",
+        fontWeight: 550,
+      }}
+    >
+      Lion Guide
+    </div>
+  ),
+  project: {
+    link: "https://github.com/L0stedMrlion/lion-guide",
+  },
+  chat: {
+    link: "https://discord.gg/dNrayyRZ9j",
+  },
+  docsRepositoryBase: "https://github.com/L0stedMrlion/lion-guide",
+  primaryHue: { dark: 200, light: 200 },
+  footer: {
+    text: "🦁 Mrlion",
+  },
+  head: useHead,
+  useNextSeoProps: useNextSeoProps,
+};
+
+export default config;
